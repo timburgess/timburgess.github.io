69a4f20db736acab1be8f4310a03435ae5d307ec
Tim Burgess <timburgess@mac.com>
2019-10-17 09:43:08 +1300

    Updates

---
+++ precache-manifest.e09f0125d490559ba1f78267c4454cfe.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "304eb5ecd39bd91f51fcdbedb8dfba46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dffcb2044b85ec775520",
+    "url": "/static/css/main.e96eecfd.chunk.css"
+  },
+  {
+    "revision": "5932374a9460aea2f413",
+    "url": "/static/js/2.254a0626.chunk.js"
+  },
+  {
+    "revision": "dffcb2044b85ec775520",
+    "url": "/static/js/main.8fa8ed38.chunk.js"
+  },
+  {
+    "revision": "db7c5e82739559b9202d",
+    "url": "/static/js/runtime-main.d06cc2da.js"
+  },
+  {
+    "revision": "1e3425546190e347aad5c9d3412bfb6e",
+    "url": "/static/media/2018-09-06.1e342554.png"
+  },
+  {
+    "revision": "d2fe91c03340730a20bb7f370bedfa73",
+    "url": "/static/media/2018-11-02.d2fe91c0.png"
+  },
+  {
+    "revision": "8c5f9ac25a00cba13b34c5ebf947f705",
+    "url": "/static/media/2018-11-27.8c5f9ac2.png"
+  },
+  {
+    "revision": "7085ea3d004b6e5ab9987248773dc4e8",
+    "url": "/static/media/2019-01-02.7085ea3d.png"
+  },
+  {
+    "revision": "4a9d11d276219c0d2a641ad5e2b3a877",
+    "url": "/static/media/2019-01-16.4a9d11d2.png"
+  },
+  {
+    "revision": "871565bea72651a91dc2bc1e981e3540",
+    "url": "/static/media/2019-01-23-07.871565be.png"
+  },
+  {
+    "revision": "36604502e55f4a55b63fd87d67653da7",
+    "url": "/static/media/2019-01-23.36604502.png"
+  },
+  {
+    "revision": "c2b4652e2e9a25234133017e7248d18e",
+    "url": "/static/media/2019-02-08.c2b4652e.png"
+  },
+  {
+    "revision": "00f272b13949d8070641208c05d43bcc",
+    "url": "/static/media/2019-02-12.00f272b1.png"
+  },
+  {
+    "revision": "c20d4d25c8eb9c34330b8bd10bfe70b1",
+    "url": "/static/media/2019-02-19-16.c20d4d25.png"
+  },
+  {
+    "revision": "86ea2f49aaa28b9c3b0698a696d44971",
+    "url": "/static/media/2019-02-19.86ea2f49.png"
+  },
+  {
+    "revision": "f1b39e40a456385d2d21b9c90c18aef8",
+    "url": "/static/media/2019-02-20.f1b39e40.png"
+  },
+  {
+    "revision": "b19b3dedaaafc0561a0e26054687ed15",
+    "url": "/static/media/2019-02-22.b19b3ded.png"
+  },
+  {
+    "revision": "ca360b6c4504b8464e5203bcfc146dbf",
+    "url": "/static/media/2019-03-02.ca360b6c.png"
+  },
+  {
+    "revision": "1cb44f444cc44c1b171e0786fccccffd",
+    "url": "/static/media/2019-03-06.1cb44f44.png"
+  },
+  {
+    "revision": "d72eed454f4931d91f619d91759de009",
+    "url": "/static/media/2019-03-07.d72eed45.png"
+  },
+  {
+    "revision": "ca424009b5e33a8f4742595e3f7b4a32",
+    "url": "/static/media/2019-03-08.ca424009.png"
+  },
+  {
+    "revision": "b5084150c95873d266cecb3d00b68721",
+    "url": "/static/media/2019-03-09.b5084150.png"
+  },
+  {
+    "revision": "cbeb319f04ad705466b62efa31732526",
+    "url": "/static/media/2019-03-12-18.cbeb319f.png"
+  },
+  {
+    "revision": "8967280884e684af6d16d0f244e8fa7d",
+    "url": "/static/media/2019-03-12.89672808.png"
+  },
+  {
+    "revision": "f960e529e3b0894e9c9c0dc29b11cf98",
+    "url": "/static/media/2019-03-13.f960e529.png"
+  },
+  {
+    "revision": "3c10426352c2f50de7ff19de01cdd794",
+    "url": "/static/media/2019-03-27.3c104263.png"
+  },
+  {
+    "revision": "a1d78a99b52e09dc3a2357274f8d9666",
+    "url": "/static/media/2019-08-01.a1d78a99.png"
+  },
+  {
+    "revision": "fa0bec73907c490b7c9646589f93d844",
+    "url": "/static/media/2019-10-15.fa0bec73.png"
+  }
+]);
